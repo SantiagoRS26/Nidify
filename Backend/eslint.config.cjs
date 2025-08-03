0982963c267ece213e9735db1532c4ed7fcf3b57
SantiagoRS26 <santiagors2611@gmail.com>
2025-08-03 15:27:10 -0500

    chore: set up backend linting and tooling

---
+++ Backend/eslint.config.cjs
@@ -0,0 +1,29 @@
+const js = require('@eslint/js');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const prettierPlugin = require('eslint-plugin-prettier');
+
+module.exports = [
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      ...prettierPlugin.configs.recommended.rules,
+    },
+  },
+];
