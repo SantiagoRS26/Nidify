5cc82057d759839acee7f9e5e15dbdb310f293ba
Santiago Roldan Silva <santiagors2611@gmail.com>
2025-08-10 14:13:52 -0500

    feat: 🎸 Mejora api

--- Backend/src/interfaces/middleware/validation.middleware.ts
+++ Backend/src/interfaces/middleware/validation.middleware.ts
@@ -14,7 +14,11 @@ export const validate = (schema: Schema) => {
         req.body = schema.body.parse(req.body);
       }
       if (schema.query) {
-        req.query = schema.query.parse(req.query);
+        const parsedQuery = schema.query.parse(req.query);
+        Object.keys(req.query).forEach((key) => {
+          delete (req.query as Record<string, unknown>)[key];
+        });
+        Object.assign(req.query as Record<string, unknown>, parsedQuery);
       }
       if (schema.params) {
         req.params = schema.params.parse(req.params);
