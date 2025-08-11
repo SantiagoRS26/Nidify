94fdbb6d19904413f0723461d7a1ea7d87b2cbfc
SantiagoRS26 <santiagors2611@gmail.com>
2025-08-11 15:39:12 -0500

    feat: agrega pipe currencyFormat y reemplaza pipe currency

--- Frontend/src/app/features/dashboard/summary/summary.component.ts
+++ Frontend/src/app/features/dashboard/summary/summary.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { CurrencyFormatPipe } from "../../../shared/pipes/currency-format.pipe";
 
 @Component({
   selector: "app-summary",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CurrencyFormatPipe],
   templateUrl: "./summary.component.html",
   styleUrl: "./summary.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
