f94956d057138f0263f47c444604c3d76cfc640c
SantiagoRS26 <santiagors2611@gmail.com>
2025-08-11 15:07:58 -0500

    Registra locale español y servicio de formato de moneda

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(localeEs);
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [...(appConfig.providers ?? []), { provide: LOCALE_ID, useValue: 'es-ES' }],
+}).catch((err) => console.error(err));
